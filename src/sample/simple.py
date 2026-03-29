751911b2f235b4e8896bb382b0340b9c2b62555e
Your Name <you@example.com>
2026-03-29 09:04:11 +0000

    Changed jenkinsfile

--- src/sample/simple.py
+++ src/sample/simple.py
@@ -1,11 +1,2 @@
-from flask import Flask
-
-app = Flask(__name__)
-
-@app.route("/")
-def hello():
-    return "Hello from Kubernetes!"
-
-if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000)
-
+def add_one(x: int) -> int:
+    return x + 1
